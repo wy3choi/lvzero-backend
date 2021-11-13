446b3caa6606088367086e88c77ead8c24ecc5ad
louis <dnduds@gmail.com>
2021-11-13 10:31:46 +0900

    EB 배포

--- application.py
+++ application.py
@@ -3,7 +3,6 @@ from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS
 import os
 
-
 application = Flask(__name__)
 cors = CORS(application, resources={r"/*": {"origins": "*"}})
 
